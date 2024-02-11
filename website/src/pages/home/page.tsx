46688ab49497511b88c4393d7827b534084b3675
Kailash8799 <kailashrajput8799@gmail.com>
2024-02-11 22:26:05 +0530

    added updated apk in website

--- website/src/pages/home/page.tsx
+++ website/src/pages/home/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="bg-[url('/bg.svg')] h-dvh items-center justify-center flex">
       <div className="bg-[#FE3C72] inline-block p-3 rounded-lg">
-        <a href="../../assets/app.apk" download={true} className="text-white font-bold">Download App</a>
+        <a href="app.apk" download={true} className="text-white font-bold">Download App</a>
       </div>
     </div>
   )
