7c34eeb577a5ef248d9aefa0b7eba9c076ac5d8d
Kailash8799 <kailashrajput8799@gmail.com>
2024-05-13 08:32:07 +0530

    Update apk in website

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,2 +1,3 @@
-const String baseUrl = "https://vivavox-backend.onrender.com";
+const String baseUrl = "https://vivavox-backend.azurewebsites.net";
+// const String baseUrl = "https://vivavox-backend.onrender.com";
 // const String baseUrl = "http://192.168.8.207:3000";
